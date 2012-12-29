f9fde857efcea8da46b6c8f8f9b9f1efb7e1eee9
Stanislaw Pusep <creaktive@gmail.com>
2012-12-29 20:34:24 -0200

    benchmark results

--- speedyfx.c
+++ speedyfx.c
@@ -9,13 +9,23 @@
 #include <unistd.h>
 
 /*
- * SpeedyFx algorithm: tokenize/hash large amount of strings efficiently
+ * SpeedyFx algorithm
+ * ==================
+ *
+ * Tokenize/hash large amount of strings efficiently.
  * Original Java implementation: http://www.hpl.hp.com/techreports/2008/HPL-2008-91R1.pdf
  * Ported to C by Stanislaw Pusep (https://github.com/creaktive)
  * Compile with:
- *  clang -lm -O2 -o speedyfx speedyfx.c
+ *     clang -lm -O2 -o speedyfx speedyfx.c
  * or:
- *  gcc -lm -O2 -o speedyfx speedyfx.c
+ *     gcc -lm -O2 -o speedyfx speedyfx.c
+ *
+ * Benchmark
+ * =========
+ *
+ * Test data: https://cs.fit.edu/~mmahoney/compression/enwik9.bz2
+ * Hardware: Intel(R) Xeon(R) CPU E5620 @ 2.40GHz
+ * Average feature vector build speed: 158.95 MB/s
  */
 
 #define MAP_SIZE 256
